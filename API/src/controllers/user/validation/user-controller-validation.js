e6be2778298f0c0102a60b17f61c0b2f266d13bf
Raymond van Tonder (Information Technology) <raymondvantonder@capitecbank.co.za>
2020-05-08 15:11:40 +0200

    Updated password regex

--- API/src/controllers/user/validation/user-controller-validation.js
+++ API/src/controllers/user/validation/user-controller-validation.js
@@ -7,7 +7,7 @@ exports.validate = (method) => {
         body('name', 'Name not provided').exists(),
         body('surname', 'Surname not provided').exists(),
         body('email', 'Invalid email').exists().isEmail(),
-        body('password', 'Password should be combination of one uppercase, one lower case, one special char, one digit and min 8 lenght').matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/)
+        body('password', 'Password should be combination of one uppercase, one lower case, one special char, one digit and min 8 lenght').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)
       ]
     }
 
